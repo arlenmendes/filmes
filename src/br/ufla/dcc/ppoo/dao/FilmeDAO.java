679b042bbdfab69d3082e30d636ac6cc94fcb976
Arlen Mendes <arlenmsilva@gmail.com>
2017-01-17 16:08:21 -0200

    Continuando projeto

---
+++ src/br/ufla/dcc/ppoo/dao/FilmeDAO.java
@@ -0,0 +1,35 @@
+package br.ufla.dcc.ppoo.dao;
+
+/**
+ * Interface do Data Access Object (Padrão de Projeto) da Filme
+ * @author Breno e lucas
+ */
+
+// importação das classes utilizadas
+import br.ufla.dcc.ppoo.modelo.Filme;
+import java.util.List;
+
+public interface FilmeDAO {
+    /**
+     * Adiciona um objeto Filme na Lista 
+     * de Filmes
+     */
+
+    public void adicionarFilme(Filme filme);
+    /**
+     * Retonar um ArrayList contendo
+     * objetos da classe Filme
+     */
+    public List<Filme> getListaFilmes();
+    /**
+     * Edita série a partir
+     * de um indicador da posição da Filme na Lista
+     */
+    public void editarFilme(Filme filme,int a);
+    /**
+     * Deleta série a partir
+     * de um indicador da posição da Filme na Lista
+     */
+    public void deletarFilme(Filme filme, int a);
+    
+}
---
+++ src/br/ufla/dcc/ppoo/servicos/GerenciadorFilmes.java
@@ -0,0 +1,34 @@
+package br.ufla.dcc.ppoo.servicos;
+
+import br.ufla.dcc.ppoo.dao.FilmeDAO;
+import br.ufla.dcc.ppoo.dao.lista.FilmeDAOLista;
+import br.ufla.dcc.ppoo.i18n.I18N;
+import br.ufla.dcc.ppoo.modelo.Filme;
+import java.util.List;
+
+public class GerenciadorFilmes {
+
+    private final FilmeDAO repositorioFilme;
+    
+
+    public GerenciadorFilmes() {
+        repositorioFilme
+                = FilmeDAOLista.obterInstancia();
+            }
+
+    public List<Filme> getListaFilme(){
+    return repositorioFilme.getListaFilmes();
+    }
+
+    public void cadastrarFilme(Filme filme){
+
+        repositorioFilme.adicionarFilme(filme);
+    }
+    public void editarFilme(Filme filme,int a) {
+        repositorioFilme.editarFilme(filme,a);
+    }
+    
+    public void deletarFilme (Filme filme, int a){
+        repositorioFilme.deletarFilme(filme, a);
+    }
+}
